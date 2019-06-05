5038f91279b16702ce970759354ece94a2d3622d
Kayla <6lackmacaroon@gmail.com>
2019-06-05 14:23:58 -0600

    created first endpoint - getting my error message

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const cohortRouter = require('../data/routers/cohort-router.js');
+const studentRouter = require('../data/routers/student-router.js');
+
 const server = express();
 
 server.use(express.json());
@@ -13,5 +16,8 @@ server.get('/', (req, res) => {
             res.status(500).json({ error: "i cant. even."})
       })
 });
+
+server.use('/api/cohorts', cohortRouter);
+server.use('/api/students', studentRouter);
 
 module.exports = server;
---
+++ data/routers/student-router.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const db = require('../helpers/students-model');
+
+router.get('/', (req, res) => {
+      db.find()
+      .then(res => {
+            res.status(200).json(res);
+      })
+      .catch(err => {
+            res.status(500).json({ error: 'could not get anything from the database'})
+      });
+});
+
+module.exports = router;
